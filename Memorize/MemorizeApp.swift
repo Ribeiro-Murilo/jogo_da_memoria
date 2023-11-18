dc15c2ba949d2cc7050351759f40c77e60ee9947
Murilo Ribeiro <muriloribeiro11@gmail.com>
2023-11-18 20:36:05 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ribeiro on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
