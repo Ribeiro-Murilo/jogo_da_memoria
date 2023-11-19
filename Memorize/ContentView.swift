34a91ef22c9f0f506e3ad1c44281b211724c713f
Murilo Ribeiro <muriloribeiro11@gmail.com>
2023-11-19 14:17:41 -0300

    Create cardsView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack{//ordena todos na mesma tela
+            ForEach(0..<2, content: { index in
+                CardView()
+            })
         }
+        .foregroundColor(Color.blue)
+        .font(Font.largeTitle)
         .padding()
     }
 }
+
+struct CardView: View {
+    var IsFaceUp: Bool = false
+    
+    var body: some View{
+        ZStack{// como se fosse um conteiner que chama o rectangle e o txt e coloca o de baixo na frente
+            if IsFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            }else{
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
